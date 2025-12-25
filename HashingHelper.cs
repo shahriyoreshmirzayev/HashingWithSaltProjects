40241f78cbbd29e5f1a914790714deb4ee7289d8
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-12-25 11:07:27 +0500

    added

---
+++ HashingHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography;
+
+namespace HashingWithSaltProjects;
+
+public class HashingHelper
+{
+    public static string GetHash(string input)
+    {
+        var salt = new byte[32];
+        RandomNumberGenerator.Create().GetBytes(salt);
+
+        var pbkdf2 = new Rfc2898DeriveBytes(input, salt, 50000, HashAlgorithmName.SHA256);
+
+        byte[] hash = pbkdf2.GetBytes(32);
+
+        byte[] hashBytes = new byte[64];
+        Array.Copy(salt, 0, hashBytes, 0, 32);
+        Array.Copy(hash, 0, hashBytes, 32, 32);
+
+        string hashedPassword = Convert.ToBase64String(hashBytes);
+        return hashedPassword;
+    }
+
+    public static bool IsHashValid(string input, string hashedValue)
+    {
+        try
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedValue);
+
+            byte[] salt = new byte[32];
+            Array.Copy(hashBytes, 0, salt, 0, 32);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(input, salt, 50000, HashAlgorithmName.SHA256);
+
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (hashBytes[i + 32] != hash[i])
+                {
+                    return false;
+                }
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HashingWithSaltProjects;
+
 Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.WriteLine("Quyidagilardan birini kiriting:");
+    Console.WriteLine("1 - Parolni HASH qilish.");
+    Console.WriteLine("2 - Parol HASH'ini tekshirish.");
+
+    string userInputOptionStr = Console.ReadLine();
+    bool isUserInputOptionValid = int.TryParse(userInputOptionStr, out int userInputOption);
+    if (!isUserInputOptionValid)
+    {
+        Console.WriteLine("Noto'g'ri son kiritildi.");
+    }
+    else
+    {
+        if (userInputOption == 1)
+        {
+            Console.WriteLine("Parolni kiriting:");
+            string hashStr = Console.ReadLine();
+
+            string hashedPassword = HashingHelper.GetHash(hashStr);
+            Console.WriteLine($"Parolning HASH-qiymati: {hashedPassword}");
+        }
+
+        if (userInputOption == 2)
+        {
+            Console.WriteLine("HASH'ni kiriting:");
+            string hashStr = Console.ReadLine();
+
+            Console.WriteLine("Solishtirmoqchi bo'lgan parolni kiriting:");
+            string input = Console.ReadLine();
+
+            bool isValid = HashingHelper.IsHashValid(input, hashStr);
+            string output = isValid ? "HASH to'g'ri!" : "HASH noto'g'ri.";
+            Console.WriteLine(output);
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine("Dastur qaytadan ishga tushadi...");
+    Console.WriteLine("\n");
+}
